387c5e071c85013fda95a654555df374f5d5534c
BenoitRanque <ranque.benoit@gmail.com>
2020-08-08 17:55:54 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# WittyWyvern.com
+
+Source code for my personal website. For now. Don't know where it will go from here
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>This website under construction</h1>
+  <h2>Please come back soon!</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
